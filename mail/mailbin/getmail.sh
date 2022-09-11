ad3a5b1afeb32bbb2852348d7d7d80b1dbd724eb
Long Li <lonuxli.64@gmail.com>
2022-09-11 17:39:17 +0000

    修改接收脚本

--- mail/mailbin/getmail.sh
+++ mail/mailbin/getmail.sh
@@ -2,4 +2,5 @@
 #export http_proxy=socks5://127.0.0.1:1080
 #export https_proxy=socks5://127.0.0.1:1080
 proxychains4 getmail -n -r ~/.mailconfig/getmail/getmailrc.gmail
-#getmail -n -r ~/.getmail/getmailrc.gmail
+#getmail -n -r ~/.mailconfig/getmail/getmailrc
+
