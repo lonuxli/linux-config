1c89302adfd1fb3cc918dc6dc0db2f1c1833db5c
Long Li <lonuxli.64@gmail.com>
2026-04-16 21:37:51 +0800

    tmux: support vm disconnect

--- tmux/plugins/tmux-plugin-sysstat/scripts/userinfo.sh
+++ tmux/plugins/tmux-plugin-sysstat/scripts/userinfo.sh
@@ -19,11 +19,35 @@ getmail(){
 	echo "${MAILBOX}(#[fg=green]${new}#[default]/${total})"
 }
 
+getvm(){
+	local vms
+	local disconnect=0
+	local lines
+	local i
+
+	readarray -t lines < <(ps ux | grep qemu- | grep -v grep)
+	vms=${#lines[@]}
+		for (( i=0; i<$vms; i++))
+		do
+			ip=`echo ${lines[i]} | awk -F "IP=" '{print $2}' | awk -F " " '{print $1}'`
+			ping ${ip} -c 1 -W 1 > /dev/null
+			if [ $? != 0 ] ;then
+				disconnect="$(( $disconnect + 1 ))"
+			fi
+		done
+
+		if [ $disconnect == 0 ] ;then
+			echo "VM:#[fg=green]${vms}#[default]"
+		else
+			echo "VM:#[fg=red]$disconnect#[default]/#[fg=green]${vms}#[default]"
+		fi
+	}
+
 main(){
 	local mailinfo=
 	mailinfo="MAIL $mailinfo $(getmail "tome")"
 	local vms=`ps ux | grep qemu | grep -v grep | wc -l`
-	local vminfo="VM:#[fg=green]${vms}#[default]"
+	local vminfo="$(getvm)"
 	echo $vminfo "|" $mailinfo
 }
 
